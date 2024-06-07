6fcf3c4c4dbf3321cf127ffd7f42fda8aaa3dba9
ExiledVisage <kerem12irmak@hotmail.com>
2024-06-08 01:23:51 +0300

    commit

--- hotelBookingAPI/obj/Debug/net8.0/hotelBookingAPI.AssemblyInfo.cs
+++ hotelBookingAPI/obj/Debug/net8.0/hotelBookingAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hotelBookingAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8c9ae1c6d8821afe2f470e22750d823002929d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcbf9a9d02c8e00901e071486d25511ea0e65573")]
 [assembly: System.Reflection.AssemblyProductAttribute("hotelBookingAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hotelBookingAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
